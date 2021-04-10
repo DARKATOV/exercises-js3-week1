c86f9f2cb0e31087f97803f6785a14a9e0fbe1cb
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-04-10 15:06:07 +0200

    finish

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -3,23 +3,117 @@ function arrow() {
   return "es6 is awesome!";
 }
 
+const arrow2 = () => {     // o const arrow2 = () => "es6 is awesome!";
+  return "es6 is awesome!"
+};
+
+console.log(arrow());
+console.log(arrow2());
+
 //Use the es6 syntax for default parameter
 function defaultParameter(name) {
   var name = name || "sam";
   return name;
 }
 
-// Use the spread operator to combine arr1 and arr2
-function combineArrays(arr1, arr2) {
-  return arr1.concat(arr2);
+
+function defaultParameter2(name='sam') {
+  return name;
 }
 
-//use destructuring to return the object's cyf property
-function destructuring(obj) {
-  return obj.cyf;
+console.log(defaultParameter());
+console.log(defaultParameter2());
+console.log(defaultParameter('hola'));
+console.log(defaultParameter2('hola'));
+
+
+// Use the spread operator to combine arr1 and arr2
+function combineArrays(array2, array3) {
+  let resultArray = [...array2,...array3];
+  return resultArray;
 }
+
+class Test {
+  constructor() {
+    this.a = 'a';
+    this.b = 'b';
+    this.c = 'c';
+    this.d = 'd';
+  }
+}
+ const p0 = new Test(); 
+ const p1 = new Test(); 
+ const p2 = new Test(); 
+ const p3  = new Test(); 
+
+const array0 = [p0,p1];
+const array1 = [p2,p3];
+const array2 = [array0,array1];
+const array3 = [...array0,...array1];
+
+console.log(array2);
+console.log(array3);
+
+console.log(combineArrays(array2,array3));
+
+
+//use destructuring to return the object's cyf property
+
+const cyfcyf = {
+  cyf : 'si',
+  cef : 'tu',
+  cof : 'mi',
+
+}
+
+function destructuring(obj) {
+  const {cyf} = obj;
+  return cyf;
+} // tambien se puede destrucuturar el objeto directamente en los aprametros de la funcion 
+
+console.log(destructuring(cyfcyf));
+
+function destructuring2({cef}) {
+  return cef;
+} // tambien se puede destrucuturar el objeto directamente en los aprametros de la funcion 
+
+console.log(destructuring2(cyfcyf));
+
+
 
 // use template literal to return a string with the sum of a and b
 function templateString(a, b) {
   return "The sum is equal to " + (a + b).toString();
-}
+};
+
+function templateString2(a, b) {
+  return `The sum is equal to ${a + b}`;
+};
+
+console.log(templateString(2,1));
+console.log(templateString2(2,1));
+
+
+
+
+// practica : como redefinir la funcion toString 
+
+// function Perro(nombre,criadero,color,sexo) {
+//   this.nombre=nombre;
+//   this.criadero=criadero;
+//   this.color=color;
+//   this.sexo=sexo;
+// }
+
+// let elPerro = new Perro("Gabby","Laboratorio","chocolate","femenino");
+
+// console.log(elPerro);
+// console.log(elPerro.toString());
+
+// Perro.prototype.toString = function perroToString() {
+//   var retorno = "Perro " + this.nombre + " es " + this.sexo + " " + this.color + " " + this.criadero;
+//   return retorno;
+// }
+
+// console.log(elPerro);
+// console.log(elPerro.toString());
--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -1,9 +1,11 @@
 class Polygon {
-  constructor(height, width) {
-    this.name = "Polygon";
+  constructor(name, height, width) {
+    this.name = name;
     this.height = height;
     this.width = width;
-  }
+    }
+  get area() {return this.width * this.height;}
+  
   sayName() {
     console.log('Hi, I am a ' + this.name);
   }
@@ -11,19 +13,18 @@ class Polygon {
 
 let p = new Polygon('Polygon', 300, 400);
 p.sayName();
-console.log('The area of this polygon is ' + p.area);
+console.log(`The area of this polygon is ${p.area}`);
 
 
 class Rectangle extends Polygon {
   constructor(height, width) {
-    super(height, width);
-    this.name = "Rectangle";
+    super("Rectangle",height, width);
     this.sides = 4;
     //this.area = this.height * this.width;
   }
   get area() {return this.width * this.height;}
   sayName() {
-    console.log('Hi I am a polygon and my name is ' + this.name + '.');
+    console.log(`Hi I am a polygon and my name is ${this.name}.`);
   }
 }
 
@@ -35,5 +36,36 @@ console.log('The area of this polygon is ' + r.area);
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
 
+class Triangle extends Polygon {
+  constructor(l3,l2,l1) {
+    super("Triangle",undefined, undefined);
+    this.l1=l1;
+    this.l2=l2;
+    this.l3=l3;
+  }
+  get area() {return this.l1 * this.l2 * this.l3;}
+  sayName() {
+    console.log(`Hi I am a polygon and my name is ${this.name}.`);
+  }
+}
 
+let t = new Triangle(50, 60, 20);
+t.sayName();
 
+console.log('The area of this polygon is ' + t.area);
+class Circle extends Polygon {
+  constructor(radius){
+  super("Circle",undefined, undefined) 
+    this.radius = radius;
+    this.pi = 3.14;
+    //this.area = this.height * this.width;
+  }
+  get area() {return this.pi * this.radius * this.radius}
+  sayName() {
+    console.log(`Hi I am a polygon and my name is ${this.name}.`);
+  }
+}
+
+let c = new Circle(10);
+c.sayName();
+console.log('The area of this polygon is ' + c.area);
